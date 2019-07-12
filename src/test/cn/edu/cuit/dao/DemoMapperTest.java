0f434ef1be96f48f355b396883b3980c6137088f
chineek <350240550@qq.com>
2019-07-12 11:21:47 +0800

    增加了Test文件夹

---
+++ src/test/cn/edu/cuit/dao/DemoMapperTest.java
@@ -0,0 +1,36 @@
+package cn.edu.cuit.dao;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import sun.plugin2.message.Message;
+
+import java.util.Date;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by 35024 on 2019/7/9.
+ */
+public class DemoMapperTest {
+    private ApplicationContext applicationContext;
+
+//    @Autowired
+//    private DemoMapper mapper;
+//
+//    @Before
+//    public void setUp() throws Exception {
+//        // 加载spring配置文件
+//        applicationContext = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
+//        // 导入需要测试的
+//        mapper = applicationContext.getBean(DemoMapper.class);
+//    }
+//
+//    @Test
+//    public void deleteByPrimaryKey() throws Exception {
+//        mapper.deleteByPrimaryKey(2);
+//    }
+
+}
