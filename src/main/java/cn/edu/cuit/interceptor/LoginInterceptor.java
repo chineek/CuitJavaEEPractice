8b962637c69bebb76037304b60b0b8ade6e40dba
chineek <350240550@qq.com>
2019-07-15 10:04:22 +0800

    增加了登录拦截器和系统首页跳转

---
+++ src/main/java/cn/edu/cuit/interceptor/LoginInterceptor.java
@@ -0,0 +1,44 @@
+package cn.edu.cuit.interceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * author: 35024
+ * date: 2019/7/15.
+ */
+public class LoginInterceptor implements HandlerInterceptor {
+
+    @Override
+    public void afterCompletion(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2, Exception arg3)
+            throws Exception {
+        // 执行完毕，返回前拦截
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2, ModelAndView arg3)
+            throws Exception {
+        // 在处理过程中，执行拦截
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object arg2) throws Exception {
+        // 在拦截点执行前拦截，如果返回true则不执行拦截点后的操作（拦截成功）
+        // 返回false则不执行拦截
+        HttpSession session = request.getSession();
+        String uri = request.getRequestURI(); // 获取登录的uri，这个是不进行拦截的
+        if (session.getAttribute("user") != null) {
+            // 登录成功不拦截
+            return true;
+        } else {
+            // 拦截后进入登录页面
+            response.sendRedirect(request.getContextPath() + "/login/log");
+            return false;
+        }
+    }
+}
+
