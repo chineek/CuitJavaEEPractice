fa96614d41310cd1edd10ea538015951e982c82e
Administrator <1043261784@qq.com>
2019-07-15 11:21:12 +0800

    增加了日期操作方法

--- src/main/webapp/asset/js/core.js
+++ src/main/webapp/asset/js/core.js
@@ -101,3 +101,42 @@ util.param = (function () {
     })();
     return paramObj;
 })();
+
+/**
+ * 日期格式化工具
+ * 参数：util.date.format(datetime,fmt)
+ */
+util.date = (function () {
+    var dateObj = {};
+
+    function Format(datetime, fmt) {
+        if (parseInt(datetime) === datetime) {
+            if (datetime.length === 10) {
+                datetime = parseInt(datetime) * 1000;
+            } else if (datetime.length === 13) {
+                datetime = parseInt(datetime);
+            }
+        }
+        datetime = new Date(datetime);
+        var o = {
+            "M+": datetime.getMonth() + 1,                 //月份
+            "d+": datetime.getDate(),                    //日
+            "h+": datetime.getHours(),                   //小时
+            "m+": datetime.getMinutes(),                 //分
+            "s+": datetime.getSeconds(),                 //秒
+            "q+": Math.floor((datetime.getMonth() + 3) / 3), //季度
+            "S": datetime.getMilliseconds()             //毫秒
+        };
+        if (/(y+)/.test(fmt))
+            fmt = fmt.replace(RegExp.$1, (datetime.getFullYear() + "").substr(4 - RegExp.$1.length));
+        for (var k in o)
+            if (new RegExp("(" + k + ")").test(fmt))
+                fmt = fmt.replace(RegExp.$1, (RegExp.$1.length === 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+        return fmt;
+    }
+
+    dateObj.format = function (datetime, fmt) {
+        return Format(datetime, fmt);
+    };
+    return dateObj;
+})();
